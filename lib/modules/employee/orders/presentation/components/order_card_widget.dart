72873a7cfb0fe32ed9075e27a9285ddc6fb3cc69
andreysales <andrey-resume@outlook.com>
2024-12-16 13:59:03 -0300

    init orders card

---
+++ lib/modules/employee/orders/presentation/components/order_card_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OrderCardWidget extends StatelessWidget {
+  const OrderCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

6df20061a02eb4753e53007457ddf38f3d378084
andreysales <andrey-resume@outlook.com>
2024-12-26 20:31:39 -0300

    button action

--- lib/modules/employee/details/presentation/details_page.dart
+++ lib/modules/employee/details/presentation/details_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:marcenaria/core/data/enums/order_status_enum.dart';
 import 'package:marcenaria/core/themes/color_theme.dart';
 import 'package:marcenaria/modules/employee/details/presentation/components/details_card_widget.dart';
+import 'package:marcenaria/modules/employee/details/presentation/components/details_historic_button_widget.dart';
 import 'package:marcenaria/modules/employee/details/presentation/components/indicator_step_widget.dart';
 
 import 'components/details_back_button_widget.dart';
@@ -24,10 +25,12 @@ class _DetailsPageState extends State<DetailsPage> {
         body: Container(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
           child: Column(
+              spacing: 20.0,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 DetailsCardWidget(),
-                const SizedBox(height: 40),
-                IndicatorStepWidget(current: OrderStatus.start)
+                IndicatorStepWidget(current: OrderStatus.start),
+                if(OrderStatus.completed == OrderStatus.completed) DetailsHistoricButtonWidget()
               ]),
         ));
   }
